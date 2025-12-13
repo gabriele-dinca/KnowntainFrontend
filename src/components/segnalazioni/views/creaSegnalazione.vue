c1b33ac7795a49f6838af216b3973b4c31f8e27b
Nikeux <nicolasaresu05@gmail.com>
2025-12-13 16:20:41 +0100

    Sviluppo pagina creaSegnalazione, manca la mappa

--- src/components/segnalazioni/views/creaSegnalazione.vue
+++ src/components/segnalazioni/views/creaSegnalazione.vue
@@ -1,11 +1,19 @@
 <script setup>
-    import { ref } from "vue"
+  import { ref } from "vue"
+  import vButton from "../../utils/vButton.vue";
+  import { useRouter } from "vue-router";
 
-    const titolo = ref("")
-    const testo = ref("")
+  const router = useRouter()
+  const titolo = ref("")
+  const testo = ref("")
 
-    function invia() {
-    }
+  function invia() {
+
+
+    
+
+    router.push("/segnalazioni")
+  }
 </script>
 
 <template>
@@ -15,10 +23,100 @@
   <div class="container">
 
     <input type="text" placeholder="Titolo" v-model="titolo" />
-    <input type="text" placeholder="testo" v-model="testo" />
+    <textarea
+      placeholder="Testo della segnalazione"
+      v-model="testo"
+      rows="5"
+    ></textarea>
 
     <div class="button-group">
-      <button class="btn-utente" @click="invia()">Invia</button>
+      <vButton testo="Invia" :fn="invia" />
     </div>
   </div>
 </template>
+
+<style scoped>
+.container {
+  max-width: 400px;
+  margin: 60px auto;
+  padding: 25px;
+
+  background-color: #ffffff;
+  border-radius: 10px;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+}
+
+h2 {
+  text-align: center;
+  margin-top: 80px; /* spazio dalla navbar fissa */
+  margin-bottom: 20px;
+  font-size: 24px;
+  /*color: #333;*/
+}
+
+input {
+  display: block;
+  width: 100%;
+  margin-bottom: 15px;
+  padding: 12px;
+
+  box-sizing: border-box;
+  border: 1px solid #ccc;
+  border-radius: 6px;
+
+  font-size: 14px;
+}
+
+input:focus {
+  outline: none;
+  border-color: #007bff;
+}
+
+.button-group {
+  margin-top: 20px;
+}
+
+.btn-utente {
+  width: 100%;
+  padding: 12px;
+
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 8px;
+
+  font-size: 16px;
+  font-weight: 600;
+  cursor: pointer;
+  transition: 0.2s ease;
+}
+
+.btn-utente:hover {
+  background-color: #0056b3;
+}
+
+.btn-utente:active {
+  transform: scale(0.98);
+}
+
+
+textarea {
+  display: block;
+  width: 100%;
+  margin-bottom: 15px;
+  padding: 12px;
+
+  box-sizing: border-box;
+  border: 1px solid #ccc;
+  border-radius: 6px;
+
+  font-size: 14px;
+  font-family: inherit;
+}
+
+textarea:focus {
+  outline: none;
+  border-color: #007bff;
+}
+
+</style>
